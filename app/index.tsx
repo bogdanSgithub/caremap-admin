275291966c8c4ccc119f135ee46c8aa0f873abd0
MarkLaMer <22vkp2@queensu.ca>
2026-03-17 10:26:08 -0400

    new locations

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,15 @@
-import { Redirect } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <Redirect href="/(tabs)/home"/>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>CareMap test app</Text>
+    </View>
   );
 }
