9d0d69f618d1520be8aef5df4d6632915e3a0d4f
Bogdan Feher <bogdan.feher@gmail.com>
2026-03-17 08:10:47 -0400

    fix tabs

--- app/(tabs)/home/_layout.tsx
+++ app/(tabs)/home/_layout.tsx
@@ -1,4 +1,3 @@
-import Entypo from '@expo/vector-icons/Entypo';
 import { router, Stack } from "expo-router";
 import { Pressable, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -10,8 +9,7 @@ export default function RootLayout() {
       <Stack.Screen name="index" options={{ headerTransparent: true, headerTitle: '',
         headerRight: () => 
         (<Pressable style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }} onPress={() => router.push('/create')}>
-            <Entypo name='plus' size={23}/>
-            <Text style={{ fontSize: 20, paddingRight: 4 }}>Donate</Text>
+            <Text style={{ fontSize: 20, paddingHorizontal: 10 }}>Donate Now</Text>
         </Pressable>)
         }}/>
         <Stack.Screen name="confirmation" options={{ headerTransparent: true, headerTitle: '',
